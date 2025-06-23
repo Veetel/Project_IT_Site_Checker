933779f725141750df12e85cefbcfc7fa0200b22
JB <j-baptiste.largeron@etu.uca.fr>
2025-06-23 19:15:49 +0200

    script ok

---
+++ script.py
@@ -0,0 +1,33 @@
+import requests
+import datetime
+
+user_input = input("Enter website URLs separated by commas: \n")
+urls = [] 
+
+## url.strip cut spaces around urls
+
+for raw_url in user_input.split(","):
+    url = raw_url.strip()
+    if not url.startswith("https://") and not url.startswith("http://"):
+        url = "https://" + url
+
+    urls.append(url)
+
+
+with open("log.txt","a") as file : ## Open a file and auto close it. Sililar to open() and close()
+    for url in urls:
+            
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                status =f"{url} is reachable"
+            else :
+                status =f"{url} send status code {response.status_code}"
+
+            
+        except requests.exceptions.RequestException as exception:
+            status = f"{url} unreachable "
+
+        logLine = f"[{datetime.datetime.now()}] {status}"
+        print(logLine)
+        file.write(logLine + "\n")
